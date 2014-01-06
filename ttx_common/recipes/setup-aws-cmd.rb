78b36c9fd0f238c93e3fdf78877b57584c1bd750
Vanessa Sun <vsun@connectdotz.com>
2014-01-06 16:49:18 -0500

    adjust upload directory structure

--- ttx_common/recipes/setup-aws-cmd.rb
+++ ttx_common/recipes/setup-aws-cmd.rb
@@ -2,6 +2,11 @@
 # setup the login account include shell and some commonly used tools
 #
 
+directory "/root/.aws" do
+  recursive true
+  action :create
+end
+
 template "/root/.aws/config" do
   source "aws_config.erb"
   owner 'root' and mode 0600
